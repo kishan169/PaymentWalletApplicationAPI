9dad95ff67e9b735558714067641026e30c453f3
kishan169 <kishansavaj1206@gmail.com>
2022-09-27 19:41:22 +0530

    Initial Setup and added dependencies in project

---
+++ PaymentWalletAPPAPI/src/main/java/com/masai/PaymentWalletAppapiApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentWalletAppapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentWalletAppapiApplication.class, args);
+	}
+
+}
